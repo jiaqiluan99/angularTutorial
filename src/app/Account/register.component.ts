8b4668e4f20ee39c52f7cf1ea8f19d40de032152
jiaqiluan99 <78166596+jiaqiluan99@users.noreply.github.com>
2023-04-02 23:40:39 -0400

    Register Form

--- src/app/Account/register.component.ts
+++ src/app/Account/register.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  submitted: boolean = false;
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+      this.registerForm = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required]
+      });
+  }
+
+  get registerFormControl(){
+    return this.registerForm.controls;
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if (this.registerForm.valid){
+      alert('Form Submitted Successfully');
+      console.table(this.registerForm.value);
+    }
+  }
 
 }
